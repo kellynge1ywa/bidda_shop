ab0ce9a55c381b682b9d8c644a4437ddf574e84b
kellynge1ywa <120782457+kellynge1ywa@users.noreply.github.com>
2023-09-06 11:29:57 +0300

    bidda application

--- src/app/components/products/products.component.ts
+++ src/app/components/products/products.component.ts
@@ -1,13 +1,27 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'products',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule,RouterModule],
   templateUrl: './products.component.html',
   styleUrls: ['./products.component.css']
 })
 export class ProductsComponent {
+  brandName='bida'
+  notifications=['Not','Cart','Like','Profile']
+  categories=['Electronics','Food','Clothing','Furniture','Phones']
+  products=[
+    {image:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQgj9G7kscXlUY-mC_gFJle9JSUsX9A0pWj9kI3wIbszSzuYO1SFqMYZjunMC-ieHe-mXQ&usqp=CAU',
+    pname:'Tecno',
+    price:7000},
+    {
+      image:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTRJzXDtNOqZZ89XxN_G4-Bj8yi23fqKzc1NA&usqp=CAU',
+      pname:'Tecno spark 4',
+      price:8000
+    }
+  ]
 
 }
